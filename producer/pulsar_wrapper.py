778e89398fb924807b1391bb7bb47e5c546924c2
nmartinbekier <nicolas.martin.bekier@gmail.com>
2022-05-26 12:27:27 +0000

    First version; pending global counters and Pulsar Functions processing

--- producer/pulsar_wrapper.py
+++ producer/pulsar_wrapper.py
@@ -27,10 +27,6 @@ class PulsarConnection():
         try: self.client.close()
         except Exception as e: print(f"\n*** Exception: {e} ***\n")
         
-    def callback(self, res, msg_id):
-        """ Needed for 'send_async', for when the broker receives the message """
-        return
-
     def _set_init_status(self):
         """ Identifying the status based on messages of 'initialized' topic.
         If there are no messages its because it hasn't been initialized.
@@ -201,7 +197,6 @@ class PulsarConnection():
             day_consumer.acknowledge(msg)
         except Exception as e:
             print(f"\n*** Exception receiving value from 'day_consumer': {e} ***\n")
-            day_consumer.negative_acknowledge(msg)
             day_consumer.close()
             return
         
@@ -218,66 +213,6 @@ class PulsarConnection():
     
     def get_initialized(self):
         return self.initialized
-    
-    def put_basic_repo_info(self, repo_list):
-        """ Publishes a series of (repo_id, owner, name, language) tuples in the
-        basic_repo_info topic"""
-        try:
-            topic_name = 'basic_repo_info'
-            basic_repo_producer = self.client.create_producer(
-                topic=f'persistent://{self.tenant}/{self.namespace}/{topic_name}',
-                producer_name=f'{topic_name}_prod',
-                message_routing_mode=PartitionsRoutingMode.UseSinglePartition)
-        except Exception as e:
-            print(f"\n*** Exception creating 'basic_repo_info' topic: {e} ***\n")
-            basic_repo_producer.close()
-            return
-        
-        for repo in repo_list:
-            try:
-                basic_repo_producer.send((
-                    f"({repo[0]}, '{repo[1]}', '{repo[2]}', '{repo[3]}')").encode('utf-8'))      
-            except Exception as e:
-                print(f"\n*** Exception sending date message: {e} ***\n")
-                basic_repo_producer.close()
-                return
-            
-        basic_repo_producer.close()
-        return True
-    
-    def get_basic_repo_info(self, num_repos):
-        """  pops a list with (repo id, owner, name, language) tuples from the topic """
-        # Create a consumer on persistent topic, always using the same name,
-        # so it always references to the current read position
-        topic_name = 'basic_repo_info'
-        try:
-            basic_repo_info_consumer = self.client.subscribe(
-                topic=f"persistent://{self.tenant}/{self.namespace}/{topic_name}",
-                subscription_name=f'{topic_name}_sub',
-                initial_position=_pulsar.InitialPosition.Earliest)
-        except Exception as e:
-            print(f"\n*** Exception creating basic_repo_info: {e} ***\n")
-            basic_repo_info_consumer.close()
-            return
-        
-        repo_list = []
-        for i in range(num_repos):
-            try:
-                # Give up to 3 seconds to receive an answer
-                msg = basic_repo_info_consumer.receive(timeout_millis=3000)
-                # Save the string message (decode from byte value)
-                message = str(msg.value().decode())
-                # Process message and append to repo_list
-                repo_list.append(eval(message))
-                # Acknowledge that the message was received          
-                basic_repo_info_consumer.acknowledge(msg)
-            except Exception as e:
-                print(f"\n*** Exception receiving value from 'basic_repo_info': {e} ***\n")
-                print("Might have reached the limit of available repos in the topic")
-                break
-        
-        basic_repo_info_consumer.close()
-        return repo_list
     
     def put_free_token(self, token):
         """ Posts a token id string with available quota """
@@ -325,7 +260,7 @@ class PulsarConnection():
             free_token_consumer.acknowledge(msg)
         except Exception as e:
             print(f"\n*** Exception receiving value from 'free_token_consumer': {e} ***")
-            print("\nMight have run out of free tokens\n")
+            print("Might have run out of free tokens\n")
             free_token_consumer.close()
             return None
         
@@ -378,13 +313,189 @@ class PulsarConnection():
             standby_token_consumer.acknowledge(msg)
         except Exception as e:
             print(f"\n*** Exception receiving value from 'standby_token_consumer': {e} ***")
-            print("\nMight have run out of standby tokens\n")
+            print("Might have run out of standby tokens\n")
             standby_token_consumer.close()
             return None
         
         standby_token_consumer.close()
         return token
+    
+    def put_basic_repo_info(self, repo_list):
+        """ Publishes a series of (repo_id, 'owner', 'name', 'language') tuples in the
+        basic_repo_info topic"""
+        try:
+            topic_name = 'basic_repo_info'
+            basic_repo_producer = self.client.create_producer(
+                topic=f'persistent://{self.tenant}/{self.namespace}/{topic_name}',
+                producer_name=f'{topic_name}_prod',
+                message_routing_mode=PartitionsRoutingMode.UseSinglePartition)
+        except Exception as e:
+            print(f"\n*** Exception creating 'basic_repo_info' topic: {e} ***\n")
+            basic_repo_producer.close()
+            return
+        
+        for repo in repo_list:
+            try:
+                basic_repo_producer.send((
+                    f"({repo[0]}, '{repo[1]}', '{repo[2]}', '{repo[3]}')").encode('utf-8'))      
+            except Exception as e:
+                print(f"\n*** Exception sending basic_repo_info message: {e} ***\n")
+                basic_repo_producer.close()
+                return
+            
+        basic_repo_producer.close()
+        return True
+    
+    def get_basic_repo_info(self, num_repos):
+        """  pops a num_repos sized list with (repo id, 'owner', 'name', language') 
+        tuples from the topic basic_repo_info. Might have less elements if the
+        topic doesn't has more repos to return"""
+        # Create a consumer on persistent topic, always using the same name,
+        # so it always references to the current read position
+        topic_name = 'basic_repo_info'
+        try:
+            basic_repo_info_consumer = self.client.subscribe(
+                topic=f"persistent://{self.tenant}/{self.namespace}/{topic_name}",
+                subscription_name=f'{topic_name}_sub',
+                initial_position=_pulsar.InitialPosition.Earliest)
+        except Exception as e:
+            print(f"\n*** Exception creating basic_repo_info: {e} ***\n")
+            basic_repo_info_consumer.close()
+            return
+        
+        repo_list = []
+        for i in range(num_repos):
+            try:
+                # Give up to 3 seconds to receive an answer
+                msg = basic_repo_info_consumer.receive(timeout_millis=3000)
+                # Save the string message (decode from byte value)
+                message = str(msg.value().decode())
+                # Process message and append to repo_list
+                repo_list.append(eval(message))
+                # Acknowledge that the message was received          
+                basic_repo_info_consumer.acknowledge(msg)
+            except Exception as e:
+                print(f"\n*** Exception receiving value from 'basic_repo_info': {e} ***")
+                print("Might have reached the limit of available repos in the topic\n")
+                break
+        
+        basic_repo_info_consumer.close()
+        return repo_list
 
+    def put_commit_repo_info(self, repo_list):
+        """ Publishes a series of (repo_id, num_commits) tuples in the
+        commit_repo_info topic"""
+        try:
+            topic_name = 'commit_repo_info'
+            commit_repo_producer = self.client.create_producer(
+                topic=f'persistent://{self.tenant}/{self.static_namespace}/{topic_name}',
+                producer_name=f'{topic_name}_prod',
+                message_routing_mode=PartitionsRoutingMode.UseSinglePartition)
+        except Exception as e:
+            print(f"\n*** Exception creating 'commit_repo_info' topic: {e} ***\n")
+            commit_repo_producer.close()
+            return
+        
+        for repo in repo_list:
+            try:
+                commit_repo_producer.send((
+                    f"({repo[0]}, {repo[1]})").encode('utf-8'))      
+            except Exception as e:
+                print(f"\n*** Exception sending commit_repo_info message: {e} ***\n")
+                commit_repo_producer.close()
+                return
+            
+        commit_repo_producer.close()
+        return True
+    
+    def put_repo_with_tests(self, repo_list):
+        """ Publishes a series of (repo_id, 'language') tuples in the
+        repo_with_tests topic """
+        try:
+            topic_name = 'repo_with_tests'
+            repo_with_tests_producer = self.client.create_producer(
+                topic=f'persistent://{self.tenant}/{self.namespace}/{topic_name}',
+                producer_name=f'{topic_name}_prod',
+                message_routing_mode=PartitionsRoutingMode.UseSinglePartition)
+        except Exception as e:
+            print(f"\n*** Exception creating 'repo_with_tests' topic: {e} ***\n")
+            repo_with_tests_producer.close()
+            return
+        
+        for repo in repo_list:
+            try:
+                repo_with_tests_producer.send((
+                    f"({repo[0]}, '{repo[1]}')").encode('utf-8'))      
+            except Exception as e:
+                print(f"\n*** Exception sending repo_with_tests message: {e} ***\n")
+                repo_with_tests_producer.close()
+                return
+            
+        repo_with_tests_producer.close()
+        return True
+    
+    def get_repo_with_tests(self, num_repos):
+        """  pops a num_repos sized list with (repo_id, 'language') tuples
+        from the topic repo_with_tests. Might have less elements if the
+        topic doesn't has more repos to return """
+        # Create a consumer on persistent topic, always using the same name,
+        # so it always references to the current read position
+        topic_name = 'repo_with_tests'
+        try:
+            repo_with_tests_consumer = self.client.subscribe(
+                topic=f"persistent://{self.tenant}/{self.namespace}/{topic_name}",
+                subscription_name=f'{topic_name}_sub',
+                initial_position=_pulsar.InitialPosition.Earliest)
+        except Exception as e:
+            print(f"\n*** Exception creating repo_with_tests: {e} ***\n")
+            repo_with_tests_consumer.close()
+            return
+        
+        repo_list = []
+        for i in range(num_repos):
+            try:
+                # Give up to 3 seconds to receive an answer
+                msg = repo_with_tests_consumer.receive(timeout_millis=3000)
+                # Save the string message (decode from byte value)
+                message = str(msg.value().decode())
+                # Process message and append to repo_list
+                repo_list.append(eval(message))
+                # Acknowledge that the message was received          
+                repo_with_tests_consumer.acknowledge(msg)
+            except Exception as e:
+                print(f"\n*** Exception receiving value from 'repo_with_tests_consumer': {e} ***")
+                print("Might have reached the limit of available repos in the topic\n")
+                break
+        
+        repo_with_tests_consumer.close()
+        return repo_list
+
+    def put_repo_with_ci(self, repo_list):
+        """ Publishes a series of (repo_id, 'language') tuples in the
+        repo_with_ci topic """
+        try:
+            topic_name = 'repo_with_ci'
+            repo_with_ci_producer = self.client.create_producer(
+                topic=f'persistent://{self.tenant}/{self.static_namespace}/{topic_name}',
+                producer_name=f'{topic_name}_prod',
+                message_routing_mode=PartitionsRoutingMode.UseSinglePartition)
+        except Exception as e:
+            print(f"\n*** Exception creating 'repo_with_ci_producer' topic: {e} ***\n")
+            repo_with_ci_producer.close()
+            return
+        
+        for repo in repo_list:
+            try:
+                repo_with_ci_producer.send((
+                    f"({repo[0]}, '{repo[1]}')").encode('utf-8'))      
+            except Exception as e:
+                print(f"\n*** Exception sending 'repo_with_ci_producer' message: {e} ***\n")
+                repo_with_ci_producer.close()
+                return
+            
+        repo_with_ci_producer.close()
+        return True
+    
 """
 # Test code
 
@@ -399,14 +510,22 @@ token = my_pulsar.get_free_token()
 
 my_pulsar.load_all_git_tokens()
 
+# Test data
 repo_list = [
     (1, 'owner_1', 'repo_1', 'language_1'),
     (2, 'owner_2', 'repo_2', 'language_2'),
     (3, 'owner_3', 'repo_3', 'language_1')]
+commit_repo_list = [(1, 15),(2, 20),(3, 25)]
+repos_with_tests_list = [(1, 'Python'),(2, 'C#'),(3, 'Javascript')]
+repos_with_ci_list = [(10, 'Lisp'),(11, 'Java'),(12, 'Go')]
 
 my_pulsar.put_basic_repo_info(repo_list)
-
 received_repo_list = my_pulsar.get_basic_repo_info(2)
+
+my_pulsar.put_commit_repo_info(commit_repo_list)
+
+my_pulsar.put_repo_with_ci(repos_with_ci_list)
+
 
 my_pulsar.close()
 """
