7111db343246767a9022d0e528783cdd0abc139c
nmartinbekier <nicolas.martin.bekier@gmail.com>
2022-05-27 11:53:37 +0000

    Pulsar Functions starting to work

--- producer/aggregate_functions.py
+++ producer/aggregate_functions.py
@@ -26,38 +26,38 @@ class AggregateFunction(Function):
         
         in_topic = context.get_current_message_topic_name()
         if 'basic_repo_info' in in_topic:
-            message = eval(item.decode()) # convert byte to text, and then to tuple
-            repo_id = message(0)
             # basic_repo_info: (repo_id, 'owner', 'name', 'language')
-            context.incr_counter(repo_id, 1) # register we've reviewed repo_id
-            if (context.get_counter(repo_id) == 1):
+            message = eval(item) # convert byte to text, and then to tuple
+            repo_id = str(message[0])            
+            context.incr_counter(f'{repo_id}', 1) # register we've reviewed repo_id
+            if (context.get_counter(f'{repo_id}') == 1):
                 # Increase the language counter if the repo hasn't been processed before
-                language_repos = f"{message(3)}-repos"
-                context.incr_counter(language_repos, 1)
-                if (context.get_counter(language_repos) == 1):
+                language_repos = f"{message[3]}-repos"
+                context.incr_counter(f'{language_repos}', 1)
+                if (context.get_counter(f'{language_repos}') == 1):
                     # If its the first time we see the language publish it to 'languages' topic
                     context.publish(
                         topic_name=f"persistent://{self.tenant}/{self.namespace}/languages",
-                        message=({message(3)}).encode('utf-8'))
+                        message=(message[3]).encode('utf-8'))
         elif 'repo_with_tests' in in_topic:
-            message = eval(item.decode())
-            repo_id = message(0)
+            message = eval(item)
+            repo_id = str(message[0])
             # repo_with_tests: (repo_id, 'language')
             repo_id_tests = f"{repo_id}-tests"
-            context.incr_counter(repo_id_tets, 1)
-            if (context.get_counter(repo_id_tests == 1)):
+            context.incr_counter(f'{repo_id_tets}', 1)
+            if (context.get_counter(f'{repo_id_tests}' == 1)):
                 # Increase counter if the repo hasn't been processed before for tests
-                language_tests = f"{message(2)}-tests"
-                context.incr_counter(language_tests, 1)
+                language_tests = f"{message[2]}-tests"
+                context.incr_counter(f'{language_tests}', 1)
         elif 'repo_with_ci' in in_topic:
-            message = eval(item.decode())
-            repo_id = message(0)
+            message = eval(item)
+            repo_id = str(message[0])
             # repo_wit_ci: (repo_id, 'language')
             # This time there's no need to check if the repo has been processed multiple times
-            language_ci = f"{message(2)}-ci"
-            context.incr_counter(language_ci, 1)
+            language_ci = f"{message[2]}-ci"
+            context.incr_counter(f'{language_ci}', 1)
         elif 'aggregate_languages_info' in in_topic:
-            language = item.decode()
+            language = item
             num_repos = context.get_counter(f"{language}-repos")
             num_tests = context.get_counter(f"{language}-tests")
             num_cis = context.get_counter(f"{language}-ci")
